1ac738e5c97b8bf8631a4cabda7327d62abdd700
JuanJDlp <juanjosedelapava1@gmail.com>
2024-01-06 21:12:50 -0500

    Created Folder Structure for the backend

---
+++ icesipalace-backend/src/test/java/com/equipodiscreto/IcesiPalace/IcesipalaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipodiscreto.IcesiPalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcesipalaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
