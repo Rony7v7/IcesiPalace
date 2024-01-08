36e8a3a52a0e7c36981cb598bb5f6b4579be8216
David Artunduaga <1040871155@u.icesi.edu.co>
2024-01-07 22:19:08 -0500

    advances in Register

---
+++ icesipalace-frontend/src/components/Register.jsx
@@ -0,0 +1,22 @@
+import { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from './api/axios';
+
+
+// para validar el nombre de usuarion y la contrasenia
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const Register = () => { 
+    const userRef = userRef();
+    const err
+    retunr (
+        <div>
+
+        </div>
+    )
+}
+
+
+
