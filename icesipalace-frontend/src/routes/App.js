250b33afa651021fc90ddca8f8fd933ef954c562
Rony Farid Ordoñez García <ronyfarido@gmail.com>
2024-01-07 20:33:54 -0500

    fix cumpilation errors

---
+++ icesipalace-frontend/src/routes/App.js
@@ -0,0 +1,33 @@
+import '../styles/App.css';
+import React, { useState } from 'react';
+import Login from '../components/login';
+
+function App() {
+
+  // Estado principal de la aplicación
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  // Función para manejar el inicio de sesión
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  // Función para manejar el cierre de sesión
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {isLoggedIn ? (
+          <button onClick={handleLogout}>Cerrar sesión</button>
+        ) : (
+          <Login handleLogin={handleLogin} />
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
