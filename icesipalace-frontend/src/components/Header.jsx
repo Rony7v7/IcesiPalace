f2966bac65c5d5c16d0f30b60459873175f32ea0
JuanJDlp <juanjosedelapava1@gmail.com>
2024-01-15 10:48:16 -0500

    When user logs in, the nav bar changes

--- icesipalace-frontend/src/components/Header.jsx
+++ icesipalace-frontend/src/components/Header.jsx
@@ -9,9 +9,21 @@ import {
   NavLink,
 } from 'reactstrap';
 import "../styles/Header.css";
+import AuthService from '../services/AuthService';
+import { useEffect } from 'react';
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  useEffect(() => {
+    const user = AuthService.getCurrentUser();
+    console.log(user);
+
+    if (user) {
+      setCurrentUser(user);
+    }
+  }, []);
 
   const toggle = () => setIsOpen(!isOpen);
 
@@ -22,12 +34,23 @@ function Header() {
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="me-auto" navbar>
-            <NavItem>
-              <NavLink href="/login">Login</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href='/register'>Register</NavLink>
-            </NavItem>
+            {currentUser ? (
+              <>
+                <NavItem>
+                  <NavLink href="#">{"Welcome"}</NavLink>
+                </NavItem>
+                {/* Add other authenticated user links here */}
+              </>
+            ) : (
+              <>
+                <NavItem>
+                  <NavLink href="/login">Login</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href='/register'>Register</NavLink>
+                </NavItem>
+              </>
+            )}
           </Nav>
         </Collapse>
       </Navbar>
